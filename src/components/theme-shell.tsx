2e6e142ef75c95f10c201152f4822ab2ffcb4f95
serErick2026 <erickson.glodo@deped.gov.ph>
2026-08-23 14:40:42 +0800

    fix: remove forge background sparks animation

--- src/components/theme-shell.tsx
+++ src/components/theme-shell.tsx
@@ -1,18 +1,10 @@
-"use client";
-
 import { useEffect } from "react";
 import { applyTheme, getTheme } from "@/lib/themes";
-import ForgeBackground from "@/components/forge-background";
 
 export default function ThemeShell({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     applyTheme(getTheme("forge"));
   }, []);
 
-  return (
-    <>
-      <ForgeBackground />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
